ea108ecaa6131fc2c0613be462fdd86d7522d7a1
Ho Luc Quyen <holucquyen04@gmail.com>
2025-09-29 13:33:17 +0700

    wtf

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./scss/volt.scss";
 // import "./assets/scss/volt.scss";
 
 createRoot(document.getElementById("root")).render(
